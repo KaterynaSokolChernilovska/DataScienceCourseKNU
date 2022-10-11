efd15cdfe6a308ba1e932630bb2a1d8762ce9154
katerynaso <fedorlidarenko@MacBook-Air-katrin.local>
2022-10-11 17:01:01 +0300

    1 task.R

--- 1 task.R
+++ 1 task.R
@@ -1,23 +1,14 @@
-Task 1
-Створити змінні базових (atomic) типів. Базові типи: character, numeric, integer, complex, logical.
-
-character
 ch <- 'a'
 print(class(ch))
-[1] "character"
-numeric
+
 n <- 78.9
 print(class(n))
-[1] "numeric"
-integer
+
 i <- 789L
 print(class(i))
-[1] "integer"
-complex
+
 c <- 78+9i
 print(class(c))
-[1] "complex"
-logical
+
 l <- TRUE
 print(class(l))
-[1] "logical"
