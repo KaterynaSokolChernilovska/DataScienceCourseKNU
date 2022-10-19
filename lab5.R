1b559ce81fad1aacf0e4ee6bd704e3b584daabd7
katerynaso <fedorlidarenko@MacBook-Air-katrin.local>
2022-10-19 16:26:27 +0300

    lab5.R

---
+++ lab5.R
@@ -0,0 +1,26 @@
+#use funtions from file pmean.R
+source('pmean.R')
+
+#Task 1
+print(pmean('specdata', 'sulfate', 1:10))
+print(pmean('specdata', 'sulfate', 55))
+print(pmean('specdata', 'nitrate'))
+
+#Task 2
+print(complete('specdata', 1))
+print(complete('specdata', c(2, 4, 8, 10, 12)))
+print(complete('specdata', 50:60))
+
+#Task 3
+cr <- corr('specdata', 150)
+print(head(cr)) 
+print(summary(cr))
+
+cr <- corr('specdata', 400)
+print(head(cr)) 
+print(summary(cr))
+
+cr <- corr('specdata', 5000)
+print(head(cr)) 
+print(summary(cr))
+print(length(cr))
